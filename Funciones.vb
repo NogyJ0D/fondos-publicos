3546d0940faee83443e20a39316425ced75330f4
Valentin Giarra <valentingiarra2@gmail.com>
2022-05-26 14:18:43 -0300

    S1 archivo de funciones

---
+++ Funciones.vb
@@ -0,0 +1,12 @@
+﻿Module Funciones
+  Public Function GenerarCaptcha(ByRef len As Integer, ByRef upper As Boolean) As String
+    Dim rand As New Random()
+    Dim allowableChars() As Char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ0123456789".ToCharArray()
+    Dim final As String = String.Empty
+    For i As Integer = 0 To len - 1
+      final += allowableChars(rand.Next(allowableChars.Length - 1))
+    Next
+
+    Return IIf(upper, final.ToUpper(), final)
+  End Function
+End Module
