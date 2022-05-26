74220594e5cd08c99ac774119f92b5ec10b836c3
Valentin Giarra <valentingiarra2@gmail.com>
2022-05-26 14:34:22 -0300

    s1 fix

--- FrmLogin.vb
+++ FrmLogin.vb
@@ -1,11 +1,11 @@
 ﻿Public Class FrmLogin
   ' Load
   Private Sub FrmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-    LblLCaptcha.Text = GenerateRandomString(6, False)
+    LblLCaptcha.Text = GenerarCaptcha(6, False)
   End Sub
   ' Controles
   Private Sub BtnLCaptcha_Click(sender As Object, e As EventArgs) Handles BtnLCaptcha.Click
-    LblLCaptcha.Text = GenerateRandomString(6, False)
+    LblLCaptcha.Text = GenerarCaptcha(6, False)
   End Sub
   Private Sub BtnLogin_Click(sender As Object, e As EventArgs) Handles BtnLogin.Click
     If InpLCaptcha.Text = LblLCaptcha.Text Then
@@ -14,14 +14,4 @@
     End If
   End Sub
   ' Funciones
-  Public Function GenerateRandomString(ByRef len As Integer, ByRef upper As Boolean) As String
-    Dim rand As New Random()
-    Dim allowableChars() As Char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLOMNOPQRSTUVWXYZ0123456789".ToCharArray()
-    Dim final As String = String.Empty
-    For i As Integer = 0 To len - 1
-      final += allowableChars(rand.Next(allowableChars.Length - 1))
-    Next
-
-    Return IIf(upper, final.ToUpper(), final)
-  End Function
 End Class
