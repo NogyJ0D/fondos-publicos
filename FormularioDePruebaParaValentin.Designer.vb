651a42398caf50611f8bc00129015a65340437df
MatiasObuljen <matiasobuljen@gmail.com>
2022-05-30 11:37:44 -0300

    frm Valen

--- FormularioDePruebaParaValentin.Designer.vb
+++ FormularioDePruebaParaValentin.Designer.vb
@@ -22,9 +22,31 @@ Partial Class FormularioDePruebaParaValentin
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(656, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ESPERO QUE ESTO EMPIECE A FUNCIONAR"
+        '
+        'FormularioDePruebaParaValentin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(677, 52)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FormularioDePruebaParaValentin"
         Me.Text = "FormularioDePruebaParaValentin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
 End Class
